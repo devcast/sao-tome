6789302a157e7d5dca5f46b9c09328017c6de7b4
Felquis Gimenes <frgformenton@gmail.com>
2016-04-25 22:22:56 -0300

    Add sw-toolbox Fastest strategy
    check out sw-toolbox doc about the fastest
    https://github.com/GoogleChrome/sw-toolbox/blob/3a3d1eed4534fe7a90c529b7
    5365aacdcc1b7c24/doc-pages/api.md#toolboxfastest
    
    I had an issue trying to cache google web fonts, it may be fixed in
    near future, an issue was opened in sw-toolbox repo

---
+++ sw.js
@@ -0,0 +1,37 @@
+importScripts('/bower_components/sw-toolbox/sw-toolbox.js');
+
+var VERSION = 4
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(self.skipWaiting())
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(self.clients.claim())
+});
+
+toolbox.router.get('/(images|js|css|bower_components)/*', toolbox.fastest, {
+    cache: {
+        name: 'blog-assets-' + VERSION,
+        maxEntries: 31
+    }
+});
+
+toolbox.router.get('/:slug?', toolbox.fastest, {
+    cache: {
+        name: 'blog-pages-' + VERSION,
+        maxEntries: 31
+    }
+});
+
+// Google APIs
+// currently not working as expected
+// https://github.com/GoogleChrome/sw-toolbox/issues/125
+toolbox.router.get('/*', toolbox.fastest, {
+    debug: true,
+    origin: 'https://fonts.(googleapis|gstatic).com',
+    cache: {
+        name: 'googleapi-' + VERSION,
+        maxEntries: 31,
+    }
+});
